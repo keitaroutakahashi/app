92c6e06967198600667f5b3c403212c098ce83c5
高橋 <my.address.d@gmail.com>
2016-12-21 08:35:41 +0900

    no message

--- ejs_sass_babel/gulpfile.js
+++ ejs_sass_babel/gulpfile.js
@@ -1,93 +1,16 @@
-// 共通
-const gulp = require('gulp');
-const plumber = require("gulp-plumber");
-const source = require('vinyl-source-stream');
-const browserSync = require('browser-sync');
+// import gulp from 'gulp';
+// import pleeease from 'gulp-pleeease';
 
-// EJS
-const ejs = require("gulp-ejs");
+var gulp = require('gulp');
+var pleeease = require('gulp-pleeease');
 
-// Sass
-const sass = require('gulp-sass');
-const minify = require('gulp-minify-css');
-const autoprefixer = require("gulp-autoprefixer");
-const concat = require('gulp-concat');
-
-// JS
-const browserify = require('browserify');
-const babelify = require('babelify');
-const uglify = require('gulp-uglify');
-const eslint = require('gulp-eslint');
-const streamify = require('gulp-streamify');
-const buffer = require('vinyl-buffer'); // here
-
-const path = {
-	dist: 'dist/assets/',
-	dev: 'dev/'
-}
-
-
-// BrowserSync
-gulp.task("browserSync", function () {
-  browserSync({
-    notify: false,
-    server: {
-        baseDir: 'dist/'
-    },
-    startPath: '/',
-  });
-
-  gulp.watch(path.dist + '**/', function() {
-    browserSync.reload();
-  });
-});
-
-
-// EJS
-gulp.task('ejs', () => {
-	gulp.src(
-		[path.dev + "ejs/**/*.ejs",'!' + path.dev + "ejs/**/_*.ejs"]
-	)
-	.pipe(plumber())
-	.pipe(ejs(ejs,{"ext": ".html"}))
-	.pipe(gulp.dest('dist/'))
-});
-
-
-// Sass
-gulp.task('scss', () => {
-  gulp.src(path.dev + 'scss/**/*.scss')
-  	.pipe(plumber())
-    .pipe(sass())
-    .pipe(autoprefixer({
-      browsers: ["last 2 versions", "Android >= 4","ios_saf >= 8"]
+gulp.task('css', () => {
+  gulp.src('./dev/scss/app.scss')
+    .pipe(pleeease({
+      "minifier": false,
+      "sass": true
     }))
-    .pipe(concat('app.css'))
-    .pipe(minify())
-    .pipe(gulp.dest(path.dist + 'asset/css'))
-});
+    .pipe(gulp.dest('./dist'));
+})
 
-
-// JS
-gulp.task('js', () => {
-	browserify(path.dev + 'js/app/app.js', { debug: true })
-		.transform(babelify)
-		.bundle()
-		.on("error", function (err) {
-      console.log(err.message); 
-      console.log(err.stack);
-    })
-		.pipe(source('bundle.js'))
-    .pipe(streamify(uglify()))
-		.pipe(gulp.dest(path.dist + 'js/'))
-});
-
-
-gulp.task('watch', () => {
-	gulp.watch(path.dev + 'ejs/**/*.ejs',['ejs']);
-	gulp.watch(path.dev + 'css/**/*.css',['scss']);
-	gulp.watch(path.dev + 'js/**/*.js',['js']);
-});
-
-
-gulp.task('default', ['watch', 'browserSync']);
+gulp.task('default', ['css']);
